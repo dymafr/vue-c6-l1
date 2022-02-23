59d127260e7b991852f29edef84d43d56c2520c0
dymafr <erwan1309@hotmail.com>
2022-02-23 17:18:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-rdcnkv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-rdcnkv)
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1><span>Bonjour</span> le monde !</h1>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+h1 {
+  color: red;
+  span {
+    color: blue;
+  }
+}
+</style>
