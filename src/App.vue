fe0cc2416855543b0c746d81f369320c0e1e40c9
dymafr <erwan1309@hotmail.com>
2022-02-23 17:26:29 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <button @click="toggle = !toggle">Toggle</button>
-  <h1 v-if="toggle">Hello le monde !</h1>
+  <input type="text" v-model="letter" />
+  <h1 v-if="letter === 'A'">A</h1>
+  <h1 v-else-if="letter === 'B'">B</h1>
+  <h1 v-else-if="letter === 'C'">C</h1>
+  <h1 v-else>Autre</h1>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const toggle = ref(true);
+const letter = ref('');
 </script>
 
 <style scoped lang="scss"></style>
